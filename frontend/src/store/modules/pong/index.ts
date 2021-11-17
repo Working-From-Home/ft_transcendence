2659a0c5762253691b1f9bd14d275a2348fb1262
Perso-alesanto <92.alex.ds@gmail.com>
2021-11-17 18:17:47 +0100

    added store commit

---
+++ frontend/src/store/modules/pong/index.ts
@@ -0,0 +1,14 @@
+import mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+
+export default ({
+	state() {
+		return {
+
+		};
+	},
+	mutations,
+	actions,
+	getters
+});
