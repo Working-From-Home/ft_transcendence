64c7b834b1ab43a20f59e06e8da50f743f5d5ab1
whayter <hwinston@student.42.fr>
2021-11-22 21:29:03 +0100

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes( new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
   .setTitle('Demo Api')
