09420cd35dc50649063d97e2624e44ddb6c9a604
whayter <hwinston@student.42.fr>
2021-11-25 00:35:31 +0100

    cleaned

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -1,29 +1,16 @@
 import { rm, writeFile } from 'fs.promises'
 import { join } from 'path'
-import { getConnection } from 'typeorm'
-
-// global.beforeEach(async () => {
-//     try {
-//         await rm(join(__dirname, '..', 'test.sqlite'));
-//     } catch(err) {}
-// });
-
-// global.afterEach(async () => {
-//     const conn = getConnection();
-//     await conn.close();
-// });
-
 
 global.beforeAll(async () => {
     const content = "DB_NAME=test.sqlite\nJWT_SECRET=SECRET\n";
     try {
-        await writeFile(join(__dirname, '..', '.env.test', content));
+        await writeFile(join(__dirname, '..', '.env.test'), content);
     } catch(err) {}
 })
 
 global.afterAll(async () => {
     try {
-        await rm('../.env.test');
+        await rm(join(__dirname, '..', '.env.test'));
     } catch(err) {}
 })
 
