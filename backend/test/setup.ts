07d1eb59e474b568210560c48c5f6ed26af4f505
whayter <hwinston@student.42.fr>
2021-11-25 00:40:48 +0100

    done re

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -1,16 +1,15 @@
 import { rm, writeFile } from 'fs.promises'
-import { join } from 'path'
 
 global.beforeAll(async () => {
     const content = "DB_NAME=test.sqlite\nJWT_SECRET=SECRET\n";
     try {
-        await writeFile(join(__dirname, '..', '.env.test'), content);
+        await writeFile('../.env.test', content);
     } catch(err) {}
 })
 
 global.afterAll(async () => {
     try {
-        await rm(join(__dirname, '..', '.env.test'));
+        await rm('../.env.test');
     } catch(err) {}
 })
 
