ed306d95fbfeec1658ee33922ad637c49940b2ce
Perso-alesanto <92.alex.ds@gmail.com>
2021-12-01 21:58:43 +0100

    Push real quick

--- frontend/src/store/modules/profile/actions.ts
+++ frontend/src/store/modules/profile/actions.ts
@@ -15,13 +15,18 @@ export default {
 			console.error('Error:', error);
 			throw error;
 		});
+		return context.dispatch('getAvatar', {
+			...payload,
+		});
+	},
+	async getAvatar(context: any, payload: any) {
 		const fetchData = {
 			method: 'GET',
 			headers: new Headers()
 		};
 		const newToken = 'Bearer ' + payload.token;
 		fetchData.headers.append('Authorization', newToken);
-		url += '/avatar';
+		let url = 'http://localhost:3000/users/' + payload.id + '/avatar';
 		fetch(url, fetchData)
 		.then((response) => response.blob())
 		.then(data => {
@@ -29,10 +34,34 @@ export default {
 			localStorage.setItem('avatar', URL.createObjectURL(data));
 			context.commit('initAvatar', {
 				avatar: URL.createObjectURL(data),
-			})
+			});
 		}).catch(error => {
 			console.error('Error:', error);
 			throw error;
+		});
+	},
+	async uploadProfile(context: any, payload: any) {
+		console.log('1 avatar:', payload.img);
+		console.log('2 avatar:', JSON.stringify(payload.img));
+		const fetchData = {
+			method: 'POST',
+			body: payload.img,
+			headers: new Headers()
+		};
+		const newToken = 'Bearer ' + payload.token;
+		fetchData.headers.append('Content-Type', 'png');
+		fetchData.headers.append('Authorization', newToken);
+		let url = 'http://localhost:3000/users/' + payload.id + '/avatar';
+		fetch(url, fetchData)
+		.then((response) => response.json())
+		.then(data => {
+			console.log('Success upload avatar:', data);
+		}).catch(error => {
+			console.error('Error:', error);
+			throw error;
+		});
+		return context.dispatch('getAvatar', {
+			...payload,
 		});
 	}
 };
