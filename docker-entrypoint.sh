89baecff0269007d81b0a8d6f5c7f2525ed5751a
ggjulio <juligonz@student.42.fr>
2022-01-30 15:08:35 +0100

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -x
 
 BOLD="\e[1m"
 RESET="\e[0m"
@@ -19,11 +19,13 @@ log_error(){
 	logging "${LIGHT_RED}error${RESET}" "$@" >&2
 	exit 1
 }
-
+id
 if [ "$(id -u)" = "0" ]; then
 	log_info "Switch from root:root to node:node, aka $(id -u node):$(id -g node) must be the same uid and gid from your host to work ! "
 	log_info 'If not, then provide arguments HOST_UID and HOST_GID with your uid and gid (man id)'
 	chown -R node:node /app
+	ls -l
+	id
 	exec su-exec node "$0" "$@"
 fi
 
