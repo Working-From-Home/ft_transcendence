5dbabe0a6e563f279523fdd1662ca5e7019db4d9
ggjulio <juligonz@student.42.fr>
2022-02-09 15:18:54 +0100

    .

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -11,12 +11,55 @@ const myApi: AxiosInstance = axios.create({
 	withCredentials: undefined
 });
 
+myApi.interceptors.request.use( config => {
+	const token = store.getters['token'];
+	if (token)
+		config.headers!['Authorization'] = 'Bearer ' + token
+	return config;
+});
+
+
+
 // interceptor to manage expired token ?
-// myApi.interceptors.request.use(function (config) {
-// 	const token = store.getState().session.token;
-// 	config.headers.Authorization =  token;
-// 	return config;
-// });
+// myApi.interceptors.response.use(
+//   response => {
+//     if (response.status === 200 || response.status === 201) {
+//       return Promise.resolve(response);
+//     } else {
+//       return Promise.reject(response);
+//     }
+//   },
+// 	error => {
+//     if (error.response.status) {
+//       switch (error.response.status) {
+//         case 400:
+
+//          //do something
+//           break;
+
+//         case 401:
+//           alert("session expired");
+//           break;        case 403:
+//           router.replace({
+//             path: "/login",
+//             query: { redirect: router.currentRoute.fullPath }
+//           });           break;        case 404:
+//           alert('page not exist');
+//           break;        case 502:
+//          setTimeout(() => {
+//             router.replace({
+//               path: "/login",
+//               query: {
+//                 redirect: router.currentRoute.fullPath
+//               }
+//             });
+//           }, 1000);
+//       }
+//       return Promise.reject(error.response);
+//     }
+//   }
+// );
+
 
 // api 42 ?
 // const ftApi = axios.create({
