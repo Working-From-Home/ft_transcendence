ea3a3b668370b161c81ed8a2179de1c36874c5e6
tlecoeuv <tanguy.lecoeuvre@gmail.com>
2022-02-25 18:13:18 +0100

    better pong styling and user info

--- frontend/src/components/pong/GameCanvas.ts
+++ frontend/src/components/pong/GameCanvas.ts
@@ -13,17 +13,11 @@ export interface IGameState {
 export class GameCanvas {
     canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D | null;
-		// private paddleHeightRatio : number;
-		// private paddleWidthRatio : number;
-		//private start: HTMLObjectElement | null;
 
     constructor(canvasName: string) {
         this.canvas = document.querySelector(canvasName) as HTMLCanvasElement;
 				console.log(this.canvas.width);
         this.context = this.canvas.getContext('2d');
-				// this.canvas.height = this.canvas.width * 0.625;	
-				// this.paddleWidthRatio = 1 / 64;
-				// this.paddleHeightRatio = 1 / 5;
     }
 
     drawGameState(gameState : IGameState) : void {
@@ -33,13 +27,6 @@ export class GameCanvas {
 			this.context?.fillRect(gameState.ball.x, gameState.ball.y, 10, 10);
 			this.context?.fillRect(gameState.leftPaddle.x, gameState.leftPaddle.y, 10, 80);
 			this.context?.fillRect(gameState.rightPaddle.x, gameState.rightPaddle.y, 10, 80);
-			// this.context?.clearRect(0,0, this.canvas.width, this.canvas.height);
-			// this.context?.fillRect(gameState.ball.x, gameState.ball.y, ballSize, ballSize);
-			// this.context?.fillRect(gameState.leftPaddle.x, gameState.leftPaddle.y, paddleWidth, paddleHeight);
-			// this.context?.fillRect(gameState.rightPaddle.x, gameState.rightPaddle.y, paddleWidth, paddleHeight);
-			// const paddleHeight = this.paddleHeightRatio * this.canvas.height;
-			// const paddleWidth = this.paddleWidthRatio * this.canvas.width;
-			// const ballSize = paddleWidth;
     }
 
 		clear() {
